d1c761650c9e13df1de1cbd1a432a2c11b22a9b6
orlando <orlandoaliocha@gmail.com>
2022-03-30 17:32:06 -0300

    First commit estrutura de dados

---
+++ projetoatribuicaoreferencia/src/com/projeto/atribbuicaoreferencia/MeuObj.java
@@ -0,0 +1,19 @@
+package com.projeto.atribbuicaoreferencia;
+
+public class MeuObj {
+
+    Integer num;
+
+    public MeuObj(Integer num) {
+        this.num = num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return num.toString();}
+    }
+
